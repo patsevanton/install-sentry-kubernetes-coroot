0f1fd849c164ba9a49cc8d649a6e02e9815fa22c
Anton Patsev <patsev.anton@gmail.com>
2024-08-26 14:43:45 +0600

    use managed postgresql

--- kubernetes/main.tf
+++ kubernetes/main.tf
@@ -145,3 +145,58 @@ resource "helm_release" "ingress_nginx" {
   }
 
 }
+
+####### postgresql
+
+module "db" {
+  source = "git::https://github.com/terraform-yc-modules/terraform-yc-postgresql.git"
+
+  folder_id  = data.yandex_client_config.client.folder_id
+  network_id = module.network.vpc_id
+  name       = "sentry-postgresql"
+
+  hosts_definition = [
+    {
+      zone             = "ru-central1-a"
+      assign_public_ip = true
+      subnet_id        = module.network.private_subnets_ids[0]
+    }
+  ]
+
+  postgresql_config = {
+    max_connections = 395
+  }
+
+  databases = [
+    {
+      name       = "sentry"
+      owner      = "sentry"
+      lc_collate = "ru_RU.UTF-8"
+      lc_type    = "ru_RU.UTF-8"
+      extensions = ["citext"]
+    }
+  ]
+
+  owners = [
+    {
+      name     = "sentry"
+      password = "sentry-postgresql-password"
+    }
+  ]
+}
+
+output "fqdn_database" {
+  value     = "c-${module.db.cluster_id}.rw.mdb.yandexcloud.net"
+  sensitive = false
+}
+
+output "owners_data" {
+  description = "List of owners with passwords."
+  sensitive   = true
+  value       = module.db.owners_data
+}
+
+output "databases" {
+  description = "List of databases names."
+  value       = module.db.databases
+}
